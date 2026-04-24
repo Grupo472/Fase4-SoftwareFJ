9e8ff6eb4d651e7f04bc0671d0b567d091e3f605
Edisson Parrado <edissonparrado97@gmail.com>
2026-04-24 17:48:09 -0500

    Se desarrolla el encabezado del codigo

---
+++ Sistema_Gestion.py
@@ -0,0 +1,8 @@
+#=======================================
+#  Sistema Integral de Gestión de Clientes, Servicios y Reservas
+#  Fase 4 - Programación 213023 - UNAD - Grupo 472
+# Desarrollado por:    Edisson Ferney Parrado Reyes
+#                                Alexandra Tautiva Betancur
+#                                
+
+#=======================================
