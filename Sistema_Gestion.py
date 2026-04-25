7284ef2a640134961a99a993eaaa99d8697beb99
Edisson Parrado <edissonparrado97@gmail.com>
2026-04-25 00:34:40 -0500

    Agregue las importaciones de biblioteca necesarias para la funcionalidad principal.

--- Sistema_Gestion.py
+++ Sistema_Gestion.py
@@ -6,4 +6,14 @@
 # Alexandra Tautiva Betancur
 # Daniel Eduardo Caro Rodriguez
 # Hugo Enrique Florez Granados
-#=======================================
+#==============================================================================
+# Se importan las librerías necesarias para el funcionamiento del sistema, incluyendo re para expresiones regulares, 
+# uuid para generación de identificadores únicos, logging para registro de eventos, 
+# os para operaciones del sistema operativo, abc para clases abstractas y datetime para manejo de fechas y horas.
+#==============================================================================
+import re
+import uuid
+import logging
+import os
+from abc import ABC, abstractmethod
+from datetime import datetime
