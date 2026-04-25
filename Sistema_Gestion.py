27eb4a02deea4b3c11ab93651aa3d3611bece6b1
Edisson Parrado <edissonparrado97@gmail.com>
2026-04-24 20:03:09 -0500

    Formato de comentarios ajustado en el archivo Sistema_Gestion.py

--- Sistema_Gestion.py
+++ Sistema_Gestion.py
@@ -1,8 +1,9 @@
 #=======================================
 #  Sistema Integral de Gestión de Clientes, Servicios y Reservas
 #  Fase 4 - Programación 213023 - UNAD - Grupo 472
-# Desarrollado por:    Edisson Ferney Parrado Reyes
-#                                Alexandra Tautiva Betancur
-#                                Daniel Eduardo Caro Rodriguez
-#                                Hugo Enrique Florez Granados
+# Desarrollado por: 
+# Edisson Ferney Parrado Reyes
+# Alexandra Tautiva Betancur
+# Daniel Eduardo Caro Rodriguez
+# Hugo Enrique Florez Granados
 #=======================================
